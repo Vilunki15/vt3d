cfa28071ffaaca78f780a58f53736dd314221ef5
Vilunki15 <ville.tanska2@gmail.com>
2023-08-17 15:35:39 +0300

    Nothing big done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { ReactDOM, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+/* import { useForm } from 'react-hook-form';
 import { FaReact } from 'react-icons/fa';
 import { useSpring, animated } from 'react-spring';
 import CookiePrompt from "./CookiePrompt";
 import Header from "./Header";
 import Main from "./Main";
-import Footer from "./Footer"
+import Footer from "./Footer" */
 
 
 import Home from "./Home";
@@ -15,29 +15,15 @@ import Websites from "./Websites";
 
 
 const App = () => {
-
-
   //const [ isCookiesAccepted, setCookiesAccepted ] = useState(false);
   //const [ isCookiesPrompted, setCookiesPrompted ] = useState(false);
-
-
-
-
-
-
-
-
-
-
   return (
     <Router>
       <div>
-        
         <Routes>
           <Route path="/" exact Component={Home} />
-          <Route path="Websites" Component={Websites} />
+          <Route path="/websites" Component={Websites} />
         </Routes>
-        <Footer />
       </div>
     </Router>
   )
